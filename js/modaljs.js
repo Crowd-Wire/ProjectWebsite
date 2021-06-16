f1c1434aff6706ccc8353ee77e95d5e5c75c82ff
mario <mariosilva@ua.pt>
2021-06-16 18:30:15 +0100

    update to project website

---
+++ js/modaljs.js
@@ -0,0 +1,150 @@
+// Get the modal
+var modal0 = document.getElementById('map-editor');
+
+// Get the button that opens the modal
+var btn0 = document.getElementById("map-editor-btn");
+
+// Get the <span> element that closes the modal
+var span0 = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn0.onclick = function() {
+    modal0.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span0.onclick = function() {
+    modal0.style.display = "none";
+}
+
+window.onclick = function(event) {
+   if (event.target == modal0) {
+       modal0.style.display = "none";
+   }
+}
+
+// Get the modal
+var modal1 = document.getElementById('prox-chat');
+
+// Get the button that opens the modal
+var btn1 = document.getElementById("prox-chat-btn");
+
+// Get the <span> element that closes the modal
+var span1 = document.getElementsByClassName("close")[1];
+
+// When the user clicks on the button, open the modal 
+btn1.onclick = function() {
+    modal1.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span1.onclick = function() {
+    modal1.style.display = "none";
+}
+
+window.onclick = function(event) {
+   if (event.target == modal1) {
+       modal1.style.display = "none";
+   }
+}
+
+// Get the modal
+var modal2 = document.getElementById('broad-cast');
+
+// Get the button that opens the modal
+var btn2 = document.getElementById("broad-cast-btn");
+
+// Get the <span> element that closes the modal
+var span2 = document.getElementsByClassName("close")[2];
+
+// When the user clicks on the button, open the modal 
+btn2.onclick = function() {
+    modal2.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span2.onclick = function() {
+    modal2.style.display = "none";
+}
+
+window.onclick = function(event) {
+   if (event.target == modal2) {
+       modal2.style.display = "none";
+   }
+}
+
+// Get the modal
+var modal3 = document.getElementById('private-worlds');
+
+// Get the button that opens the modal
+var btn3 = document.getElementById("private-worlds-btn");
+
+// Get the <span> element that closes the modal
+var span3 = document.getElementsByClassName("close")[3];
+
+// When the user clicks on the button, open the modal 
+btn3.onclick = function() {
+    modal3.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span3.onclick = function() {
+    modal3.style.display = "none";
+}
+
+window.onclick = function(event) {
+   if (event.target == modal3) {
+       modal3.style.display = "none";
+   }
+}
+
+// Get the modal
+var modal4 = document.getElementById('file-share');
+
+// Get the button that opens the modal
+var btn4 = document.getElementById("file-share-btn");
+
+// Get the <span> element that closes the modal
+var span4 = document.getElementsByClassName("close")[4];
+
+// When the user clicks on the button, open the modal 
+btn4.onclick = function() {
+    modal4.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span4.onclick = function() {
+    modal4.style.display = "none";
+}
+
+window.onclick = function(event) {
+   if (event.target == modal4) {
+       modal4.style.display = "none";
+   }
+}
+
+// Get the modal
+var modal5 = document.getElementById('mass-meet');
+
+// Get the button that opens the modal
+var btn5 = document.getElementById("mass-meet-btn");
+
+// Get the <span> element that closes the modal
+var span5 = document.getElementsByClassName("close")[5];
+
+// When the user clicks on the button, open the modal 
+btn5.onclick = function() {
+    modal5.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span5.onclick = function() {
+    modal5.style.display = "none";
+}
+
+window.onclick = function(event) {
+   if (event.target == modal5) {
+       modal5.style.display = "none";
+   }
+}
+
